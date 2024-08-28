61ebe2700fb1784d55ff36a84c38541a0e32444e
FRED KANEZA <95358548+amankanz@users.noreply.github.com>
2024-08-28 17:59:26 +0300

    add: useGeolocation custom hook

---
+++ src/useGeolocation.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export function useGeolocation(counting) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [position, setPosition] = useState({});
+  const [error, setError] = useState(null);
+
+  const { lat, lng } = position;
+
+  function getPosition() {
+    counting?.((count) => count + 1);
+
+    if (!navigator.geolocation)
+      return setError("Your browser does not support geolocation");
+
+    setIsLoading(true);
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+        setIsLoading(false);
+      },
+      (error) => {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    );
+  }
+
+  return { lat, lng, getPosition, isLoading, error };
+}
